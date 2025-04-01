8597ce3b05198b7ba44e6ed1ff9cd2c85836c792
Ege Akar <egeak@example.com>
2025-04-02 00:16:26 +0300

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    parserOptions: {
+      ecmaVersion: 2020, // ECMAScript sürümünü belirle
+      sourceType: "module", // ES6 modüllerini kullan
+    },
+  },
+  // Eğer başka özelleştirilmiş ayarları eklemek istersen:
+  // "rules" veya "env" gibi
+];
 
 export default eslintConfig;
